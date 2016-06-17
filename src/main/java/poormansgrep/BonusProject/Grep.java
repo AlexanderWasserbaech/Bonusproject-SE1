8cb8c6ac087b30af3d76fcc8b745c9d5ac96e52e
Marvin Blessing <blessima@freenet.de>
2016-06-17 17:08:28 +0200

    Grep unused imports and AppTest
    + removed unused import in Grep
    + used ctrl + shift +f to quick format in AppTest
    + removed comments on head and origin master
    + set up filecreation and filedeletion in @Before and @After
    + file can now be written to using fileoutputstream and byte[]
    + test examples written
    - still missing some other test cases

--- src/main/java/poormansgrep/BonusProject/Grep.java
+++ src/main/java/poormansgrep/BonusProject/Grep.java
@@ -6,8 +6,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.nio.file.*;
 import java.util.Scanner;
--- src/test/java/poormansgrep/BonusProject/AppTest.java
+++ src/test/java/poormansgrep/BonusProject/AppTest.java
@@ -3,69 +3,102 @@ package poormansgrep.BonusProject;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
-//<<<<<<< HEAD
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
-
-//=======
-//>>>>>>> origin/master
-
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 /**
- * Unit test for simple App.
+ * Unit testing for grep class which calls itself the search class
+ * tests are for several in and output issues
  */
 public class AppTest {
 	private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-
-	   @Before
-	   public void setUpStreams() {
-	       System.setOut(new PrintStream(outContent));
-	   }
-
-	   @After
-	   public void cleanUpStreams() {
-	       System.setOut(null);
-	       outContent.reset();
-	   }
-	   
-	@Test
-	public void createFile() throws FileNotFoundException, UnsupportedEncodingException{	
-		PrintWriter writer = new PrintWriter("index.txt", "UTF-8");
-		writer.println("the red cross");
-		writer.close();
-	}
 		
-	@Test
-	public void checkFile() throws FileNotFoundException, IOException{	
-		File file = new File ("index.txt");
-		Assert.assertEquals(file.length() == 0 && file.exists(), "index.txt");
-	}
+		static FileOutputStream fop = null;
+    	static File file;
+    	@Before
+    	public void setUpStreams() {
+    		System.setOut(new PrintStream(outContent));
+    		try {
+    			file = new File("./test.txt");
+    			fop = new FileOutputStream(file);
+    			if(!file.exists()){
+    				file.createNewFile();
+    			}
+    		} catch (IOException e){
+    			e.printStackTrace();
+    		} 
+    	}
+
+    	@After
+    	public void cleanUpStreams() {
+    		System.setOut(null);
+    		outContent.reset();
+    		try {
+    			if(fop != null){
+    				fop.close();
+    				file.delete();
+    			}
+    		} catch (IOException e){
+    			e.printStackTrace();
+    		}
+    	}
+	   
+    	@Test
+    	public void createFile() throws FileNotFoundException, UnsupportedEncodingException{	
+    		PrintWriter writer = new PrintWriter("index.txt", "UTF-8");
+    		writer.println("the red cross");
+    		writer.close();
+    	}
+		
+    	@Test
+    	public void checkFile() throws FileNotFoundException, IOException{	
+    		File file = new File ("index.txt");
+    		Assert.assertEquals(file.length() == 0 && file.exists(), "index.txt");
+    	}
 	
-	@Test 
-	public void testCommands() throws Exception{
-		String[] commands = new String [] {"-l", "-i"};
-		Assert.assertEquals(commands, outContent.toByteArray());
-	}
+    	@Test 
+    	public void testCommands() throws Exception{
+    		String[] commands = new String [] {"-l", "-i"};
+    		Assert.assertEquals(commands, outContent.toByteArray());
+    	}
 	  
     /**
      * Two testing methods testing if the output lines equals the expected string
      * @throws IOException 
      * @throws FileNotFoundException 
      */
-    @Test
-    public void testGrepReturn() throws FileNotFoundException, IOException {
-       Grep.GrepTest(new String[]{"red","D:/workspace/test.txt"});
-       Assert.assertEquals("red",outContent.toString());
-    }
-    @Test
-    public void testGrepReturn2() throws FileNotFoundException, IOException {
-    	Grep.GrepTest(new String[]{"red","D:/workspace/test2.txt"});
-        Assert.assertEquals("red\nred",outContent.toString());
-    }
+    	@Test
+    	public void testGrepReturn() throws FileNotFoundException, IOException {
+    		// to create file and write to it use this example
+    		try {
+    			byte[] byteContent = "red".getBytes();
+    			fop.write(byteContent);
+    			fop.flush();
+    			fop.close();
+    		} catch (IOException e){
+    			e.printStackTrace();
+    		}
+    		Grep.GrepTest(new String[]{"red","./test.txt"});
+    		Assert.assertEquals("red",outContent.toString());
+    	}
+    	@Test
+    	public void testGrepReturn2() throws FileNotFoundException, IOException {
+    		try {
+    			byte[] byteContent = "red\nred".getBytes();
+    			fop.write(byteContent);
+    			fop.flush();
+    			fop.close();
+    		} catch (IOException e){
+    			e.printStackTrace();
+    		}
+    		Grep.GrepTest(new String[]{"red","./test.txt"});
+    		Assert.assertEquals("red\nred",outContent.toString());
+    	}
 }
